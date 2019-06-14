06164560018a6f65d2962fbda4b62c7b41a6745f
Ashanie Cole <ashanie.cole@numerator.com>
2019-06-14 17:01:26 -0500

    update inline comment in the load_data

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -89,7 +89,7 @@ def get_filters():
 
 def load_data(city, month, day):
     """
-    Loads data for the specified city and filters by month and day if applicable.
+    Loads data for the specified city and filters by month, day or both if applicable.
 
     Args:
         (str) city - name of the city to analyze
