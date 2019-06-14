7fd68c4d967bef93e4e40cf72c7c1838ffdd6990
Ashanie Cole <ashanie.cole@numerator.com>
2019-06-14 17:03:35 -0500

    update inline comment in the user_stats

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -223,7 +223,7 @@ def trip_duration_stats(df):
 
 
 def user_stats(df):
-    """Displays statistics on bikeshare users."""
+    """Displays statistics on bikeshare users. Add DOB and Gender if the column exists"""
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
